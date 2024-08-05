1723539c99ea4367d76b16262224cf9a0676de2e
Mips2648 <mips2648@gmail.com>
2024-08-05 15:31:08 +0200

    bugfix

--- src/source_file.py
+++ src/source_file.py
@@ -19,32 +19,26 @@ class SourceFile(object):
         if text not in self._prompts:
             self._prompts[text] = Prompt(text)
 
-    def search_prompts(self, existing_translations: dict[str, Prompt]):
+    def search_prompts(self):
         patern___ = re.compile(r'__\s*\(\s*((?P<delim>["\'])(?P<texte>.*?)(?P=delim))\s*,\s*\S+\s*\)')
         try:
             content = self._file.read_text(encoding="UTF-8")
-            # with (open(self.get_absolute_path(), "r")) as f:
-            #     content = f.read()
         except Exception as ex:
             info = inspect.currentframe()
             print (ex, "( at line" , info.f_lineno , ")")
             sys.exit(1)
 
-        # Debug ("        Recherche {{..}}\n")
         for txt in re.findall("{{(.*?)}}", content):
             if len(txt) != 0:
-                # Verbose ("        " + txt)
                 self.__add_prompt(txt)
             else:
                 Warning (f"ATTENTION, il y a un texte de longueur 0 dans le fichier <{self._file.as_posix()}>")
 
         if self._file.suffix == ".php":
-            # Debug ('        Recherche __("...",__FILE__)\n')
             for match in patern___.finditer(content):
                 text = match.group('texte')
                 delim = match.group('delim')
                 regex = r'(^' + delim + r')|([^\\]' + delim + r')'
-                # Verbose ("        " + texte)
                 if re.search(regex,text):
                     print("====  Délimineur de début et fin de chaîne trouvé dans le texte !!!")
                     print(f"      Fichier: {self._file.as_uri()}")
